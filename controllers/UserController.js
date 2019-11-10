41c10345ce4bc06975d7175b39df36202ae9b07a
Rodrigo Nogueira <rdg6design@gmail.com>
2019-11-10 04:26:45 -0300

    validando formularios

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -9,8 +9,60 @@ class UserController {
         //let _this = this
         this.formEl.addEventListener("submit", event=>{
             event.preventDefault();
-            this.addLine(this.getValues())    
+
+            let values = this.getValues();
+            this.getPhoto().then(
+                (content)=>{
+
+                    values.photo = content;
+                    this.addLine(values) 
+
+                }, 
+                (e)=>{
+
+                    console.error(e)
+
+            })
+               
         }) 
+
+    }
+
+    getPhoto(){
+
+        return new Promise((resolve, reject)=>{
+        
+            let fileReader = new FileReader();
+            let elements = [...this.formEl.elements].filter(item=>{
+    
+            if(item.name === 'photo'){
+                return item;
+             }
+    
+            })
+    
+            let file = elements[0].files[0];
+    
+            //console.log(elements[0].files);
+    
+            fileReader.onload =()=>{
+                resolve(fileReader.result)
+            }
+
+            fileReader.onerror =(e)=>{
+                reject(e)
+            }
+
+            if(file){
+                fileReader.readAsDataURL(file);
+            }else{
+                resolve('dist/img/boxed-bg.jpg')
+            }
+
+            
+
+        })
+
 
     }
 
@@ -21,6 +73,8 @@ class UserController {
             if(field.name == "gender"){
                 if(field.checked)
                 user[field.name] = field.value;
+            }else if(field.name == "admin"){
+                user[field.name] = field.checked; 
             }else{
                 user[field.name] = field.value;
                 
@@ -42,22 +96,27 @@ class UserController {
                 return objectUser;
     }
     addLine(dataUser){
-        //console.log(dataUser)
-        this.tableEl.innerHTML = `
+
+        let tr = document.createElement("tr");
+
+        tr.innerHTML = `
             
-    <tr>
-        <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-        <td>${dataUser.name}</td>
-        <td>${dataUser.email}</td>
-        <td>${dataUser.admin}</td>
-        <td>${dataUser.birth}</td>
-        <td>
-          <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-          <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-        </td>
-    </tr>
-        
-    `;
+        <tr>
+            <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin ? 'Sim' : 'Não'}</td>
+            <td>${Utils.dateFormat(dataUser.register)}</td>
+            <td>
+              <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+              <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+        </tr>
+            
+        `;
+
+        //console.log(dataUser)
+        this.tableEl.appendChild(tr); 
         
     } 
 }
