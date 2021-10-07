aa93e2dd30bc63ba7cfd3a879aa5b4e41e7afffc
41RDG6BR <rdg6design@gmail.com>
2021-10-07 14:29:04 -0300

    Trabalhando com edição de Dados - Alterando dados do Objeto

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-//var nome = document.querySelector("#exampleInputName")
-//var gender = document.querySelectorAll("#form-user-create [name=gender]:checked")
-//var gender = document.querySelector("#exampleInputGenderF")
-//var birth = document.querySelector("#exampleInputBirth")
-//var country = document.querySelector("#exampleInputCountry")
-//var email = document.querySelector("exampleInputEmail")
-//var password = document.querySelector("#exampleInputPassword")
-//var photo = document.querySelector("#exampleInputFile")
-//var admin = document.querySelector("#")
-
-let userController = new UserController("form-user-create", "table-users")
-
+let userController = new UserController("form-user-create", "form-user-update", "table-users")
